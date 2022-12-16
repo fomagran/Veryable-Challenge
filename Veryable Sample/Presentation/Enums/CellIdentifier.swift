f2bab62ce38fbc9e62d1053bcab5b65af5be5d46
Fomagran <fomagran6@gmail.com>
2022-12-15 22:45:27 -0600

    [Feat] Create a CellIndentifier Enum

---
+++ Veryable Sample/Presentation/Enums/CellIdentifier.swift
@@ -0,0 +1,13 @@
+//
+//  CellIdentifier.swift
+//  Veryable Sample
+//
+//  Created by Fomagran on 2022/12/15.
+//  Copyright © 2022 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+
+enum CellIdentifier: String {
+    case AccountTableViewCell
+}
