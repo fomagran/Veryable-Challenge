a6894972455fab2abbd46f10c0675f3fecdb3710
Fomagran <fomagran6@gmail.com>
2022-12-15 22:45:58 -0600

    [Feat] Create a ImageName Enum

---
+++ Veryable Sample/Presentation/Enums/ImageName.swift
@@ -0,0 +1,16 @@
+//
+//  ImageName.swift
+//  Veryable Sample
+//
+//  Created by Fomagran on 2022/12/15.
+//  Copyright © 2022 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+
+enum ImageName: String {
+    case bank
+    case arrow
+    case card
+    case back
+}
