3b142a5474a2dc73e7155133ede73c4803460e22
Fomagran <fomagran6@gmail.com>
2022-12-15 22:56:37 -0600

    [Feat] Added Colors to ViewColor and Create a HexColor Extension

--- Veryable Sample/VryCustom/Colors/ViewColor.swift
+++ Veryable Sample/VryCustom/Colors/ViewColor.swift
@@ -10,11 +10,39 @@ import Foundation
 import UIKit
 
 public enum ViewColor: VColor {
-    case background, surface
+    case background
+    case surface
+    case primaryBlue
+    case primaryBlueDark
+    case greyDark
+    case grey
+    case greyLight
+    case greyDisabled
+    case accountName
+    case desc
+    
     var color: UIColor {
         switch self {
-            case .background: return UIColor(named: "Background")!
-            case .surface: return UIColor(named: "Surface")!
+        case .background:
+            return UIColor(named: "Background")!
+        case .surface:
+            return UIColor(named: "Surface")!
+        case .primaryBlue:
+            return UIColor(named: "PrimaryBlue")!
+        case .primaryBlueDark:
+            return UIColor(named: "PrimaryBlueDark")!
+        case .greyDark:
+            return UIColor(named: "GreyDark")!
+        case .grey:
+            return UIColor(named: "Grey")!
+        case .greyLight:
+            return UIColor(named: "GreyLight")!
+        case .greyDisabled:
+            return UIColor(named: "GreyDisabled")!
+        case .accountName:
+            return UIColor(hexString: "#4A4A4A")
+        case .desc:
+            return UIColor(hexString: "#7E7E7E")
         }
     }
 }
