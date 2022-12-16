890096b9c6caadd637037aa8d335379156e277d2
Fomagran <fomagran6@gmail.com>
2022-12-15 22:46:56 -0600

    [Feat] Create a AlertType Enum

---
+++ Veryable Sample/Presentation/Enums/AlertType.swift
@@ -0,0 +1,15 @@
+//
+//  AlertType.swift
+//  Veryable Sample
+//
+//  Created by Fomagran on 2022/12/15.
+//  Copyright © 2022 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+
+enum AlertType: String {
+    case error
+    case done
+    case back
+}
