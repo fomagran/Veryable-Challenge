a87cf7b2d4ef81b42485d1c7d91b82617db6e7ba
Fomagran <fomagran6@gmail.com>
2022-12-15 22:08:43 -0600

    [Test] Create a MockAccountService and DummyData

---
+++ Veryable SampleTests/Data/Repositories/Mocks/MockAccountService.swift
@@ -0,0 +1,26 @@
+//
+//  MockAccountService.swift
+//  Veryable SampleTests
+//
+//  Created by Fomagran on 2022/12/15.
+//  Copyright © 2022 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+@testable import Veryable_Sample
+import RxSwift
+
+class MockAccountService: AccountService {
+    var isError = false
+    
+    func request(_ request: AccountRequest) -> Observable<[Account]> {
+        return Observable<[Account]>.create { [weak self] observer in
+            if self!.isError {
+                observer.onError(NetworkError.error(reason: "Test Error"))
+            } else {
+                observer.onNext(DummyData.dummyAccounts)
+            }
+            return Disposables.create()
+        }
+    }
+}
