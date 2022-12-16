5acf50149867e2b9a60682fc13f201550774d2c6
Fomagran <fomagran6@gmail.com>
2022-12-15 22:14:49 -0600

    [Test] Create a MockAccountRequest

---
+++ Veryable SampleTests/Data/Network/Mocks/MockAccountRequest.swift
@@ -0,0 +1,14 @@
+//
+//  MockAccountRequest.swift
+//  Veryable SampleTests
+//
+//  Created by Fomagran on 2022/12/15.
+//  Copyright © 2022 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+@testable import Veryable_Sample
+
+struct InvalidURLAccountRequest: AccountRequest {
+    var url = ""
+}
